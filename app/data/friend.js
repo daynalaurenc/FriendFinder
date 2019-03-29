1db069e60a94702fb98b2e7d2c87204622eecdc9
DLC <daynalaurenc@gmail.com>
2019-03-28 18:46:53 -0700

    survey, api routes

---
+++ app/data/friend.js
@@ -0,0 +1,83 @@
+var friendsArray = [
+    {
+        name:'MBJ',
+        photo: '',
+        scores: [
+
+        ]
+    },
+    {
+        name:'LG',
+        photo: '',
+        scores: [
+
+        ]
+    },
+    {
+        name:'DK',
+        photo: '',
+        scores: [
+
+        ]
+    },
+    {
+        name:'AP',
+        photo: '',
+        scores: [
+            5,
+            3,
+            3,
+            1,
+            1,
+            2,
+            4,
+            4,
+            3,
+            1
+        ]
+    },
+    {
+        name:'',
+        photo: '',
+        scores: [
+
+        ]
+    },
+    {
+        name:'',
+        photo: '',
+        scores: [
+
+        ]
+    },
+    {
+        name:'',
+        photo: '',
+        scores: [
+
+        ]
+    },
+    {
+        name:'MJI',
+        photo: '',
+        scores: [
+
+        ]
+    },
+    {
+        name:'GIRL',
+        photo: '',
+        scores: [
+
+        ]
+    },
+    {
+        name:'DLC',
+        photo: '',
+        scores: [
+
+        ]
+    },
+];
+
+module.exports = friendsArray;
